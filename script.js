89b9767045a7143d7c38e2ad0d9499e87b51b97b
Tanny2709 <123792042+Tanny2709@users.noreply.github.com>
2025-11-11 17:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+// your JS code
+
+const line = document.getElementById("line");
+
+let angle = 0;
+
+setInterval(() => {
+  angle += 2;
+  line.style.transform = `rotate(${angle}deg) translate(-50%, -50%)`;
+}, 20);
